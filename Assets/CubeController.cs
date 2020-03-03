675cae8384b283cbd01c5ddf63da4695a1d0548d
cheng096419 <hekiya096419@hotmail.com>
2020-03-03 20:16:09 +0900

    GoForwardUnityChan Lesson7課題
    ブロック同士衝突時もしくは地面に衝突時の効果音

---
+++ Assets/CubeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+	private float speed = -12;
+	private float deadline = -10;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (this.speed * Time.deltaTime, 0, 0);
+
+		if (transform.position.x < this.deadline) {
+			Destroy (gameObject);
+		}
+	}
+
+	void OnCollisionEnter2D (Collision2D collision2d){
+		if (collision2d.gameObject.tag == "BlockTag" || collision2d.gameObject.name == "ground") {
+			GetComponent<AudioSource> ().Play ();
+		}
+	}
+}
